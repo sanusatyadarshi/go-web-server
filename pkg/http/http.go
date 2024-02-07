37ec40cf9cab4dc31ec5fa254d404ca083b99677
Sanu Satyadarshi <sanusatyadarshi@gmail.com>
2024-02-07 19:34:13 +0530

    added more packages

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -2,8 +2,26 @@ package http
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func StartServer() {
 	fmt.Println("Server Started")
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	signupHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Signup Here!\n")
+	}
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/signup", signupHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
