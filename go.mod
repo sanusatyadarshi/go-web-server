743b932ec0378080f2500834a67917625fa27d8c
Sanu Satyadarshi <sanusatyadarshi@gmail.com>
2024-02-18 14:23:35 +0530

    added credit card verification code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sanusatyadarshi/go-web-server
 
 go 1.20
+
+require github.com/theplant/luhn v0.0.0-20170224032821-81a1a381387a // indirect
---
+++ pkg/validator/validator.go
@@ -0,0 +1,47 @@
+// Test credit card numbers: https://www.paypalobjects.com/en_AU/vhelp/paypalmanager_help/credit_card_numbers.htm
+
+package validator
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/theplant/luhn"
+)
+
+type creditCardDetails struct {
+	name       string
+	cardNumber string
+	expiry     string
+}
+
+func (c creditCardDetails) checkCardLength() string {
+	cardNumber := c.cardNumber
+	if len(cardNumber) < 13 || len(cardNumber) > 19 {
+		fmt.Printf("Card number is invalid: %s\n", cardNumber)
+		fmt.Println(len(cardNumber))
+		os.Exit(1)
+	}
+	return cardNumber
+}
+
+func (c creditCardDetails) lunhValidator() bool {
+	cardNumber, err := strconv.Atoi(c.cardNumber)
+	if err != nil {
+		fmt.Printf("Failed to convert the card number %s in integer ", c.cardNumber)
+	}
+	luhn := luhn.Valid(cardNumber)
+
+	//return true or false
+	return luhn
+
+}
+
+// func main() {
+// 	cardNumber := creditCardDetails{name: "HDFC", cardNumber: "6011111111111117", expiry: "12/30"}
+
+// 	cardLength, validCardNumber := cardNumber.checkCardLength(), cardNumber.lunhValidator()
+// 	fmt.Println(cardLength, validCardNumber)
+
+// }
